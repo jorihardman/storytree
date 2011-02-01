c239ccd28e2333352b23fc5c173b6ffa9c184598
Jori Hardman <jori@u.northwestern.edu>
2011-01-31 20:46:02 -0600

    More fancy jquery animations

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,17 +1,21 @@
 $(document).ready(function() {
+  $('#branches').hide();
+  $('#branches').slideDown('fast');
+
   $('.page').click(function() {
+    leaf_id = this.id;
     $(this).animate({marginLeft: '0px'}, 'fast', function() {
-      leaf = this.id;
       $('.page').each(function() {
         if (parseInt(this.id) > parseInt(leaf)) {
           $(this).fadeOut('fast');
         }
-      })
-      window.location = '/leaves/'+this.id
+      });
+      slideAndRedirect(leaf_id);
     });
   });
 
   $('.branch').click(function() {
+    leaf_id = this.id;
     $(this).animate({marginLeft: '0px'}, 'fast', function() {
       $(this).slideUp('fast', function() {
         $(this)
@@ -19,9 +23,7 @@ $(document).ready(function() {
           .html($(this).html().replace(/\[\d+\]/, ''))
           .attr('class', 'page')
           .appendTo('#story')
-          .slideDown('fast', function () {
-            window.location = '/leaves/'+this.id;
-          });
+          .slideDown('fast', slideAndRedirect(leaf_id));
       });
     });
   });
@@ -35,3 +37,9 @@ $(document).ready(function() {
     }
   );
 });
+
+function slideAndRedirect(leaf_id) {
+  $('#branches').slideUp('slow', function() {
+    window.location = '/leaves/'+leaf_id;
+  });
+}
