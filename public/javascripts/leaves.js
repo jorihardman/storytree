3a3871e0f6303fe8a7fc722cd772d375b1c301aa
Jori Hardman <jorihardman@gmail.com>
2011-02-13 19:38:43 -0500

    changed show animations to make them a bit smoother

--- public/javascripts/leaves.js
+++ public/javascripts/leaves.js
@@ -1,22 +1,23 @@
 $(document).ready(function() {
+  window.location = '#story';
   $('#branches').slideDown('fast');
-  bindStory();
+  bindAncestors();
   bindBranches();
   bindPageLinks();
 });
 
-function bindStory() {
-  $('.page').click(function() {
+function bindAncestors() {
+  $('.ancestor').click(function() {
     leaf_id = this.id;
     $(this).animate({marginLeft: '0px'}, 'fast', function() {
-      $('html, body').animate({scrollTop:0}, 'fast', function() {
-        $('.page').each(function() {
+      $('html, body').animate({scrollTop: $('#story').offset().top}, 'fast', function() {
+        $('.ancestor').each(function() {
           if (parseInt(this.id) > parseInt(leaf_id)) {
             $(this).fadeOut('fast');
           }
         });
         $('#branches').slideUp('fast', function() {
-          window.location = leaf_id;
+          window.location = leaf_id+'#story';
         });
       });
     });
@@ -24,35 +25,36 @@ function bindStory() {
 }
 
 function bindBranches() {
-  $('.branch').live('click', function() {
+  $('.branch').click(function() {
     leaf_id = this.id;
     $(this).animate({marginLeft: '0px'}, 'fast', function() {
-      $('html, body').animate({scrollTop:0}, 'fast', function() {
+      $('html, body').animate({scrollTop: $('#story').offset().top}, 'fast', function() {
         $('#branches').slideUp('fast', function() {
           $('#'+leaf_id+' > .stats').hide()
           $('#'+leaf_id)
             .hide()
-            .attr('class', 'page')
+            .attr('class', 'ancestor')
             .appendTo('#story')
             .slideDown('fast', function() {
-              window.location = leaf_id;
+              window.location = leaf_id+'#story';
             });
         });
       });
     });
   });
 
-  $('.branch, .page').live('mouseover mouseout', function(event) {
-    if (event.type == 'mouseover') {
-      $(this).animate({marginLeft: '10px'}, 'fast');
-    } else {
+  $('.branch, .ancestor').hover(
+    function() {
+      $(this).animate({marginLeft: '5px'}, 'fast');
+    },
+    function() {
       $(this).animate({marginLeft: '0px'}, 'fast');
     }
-  });
+  );
 }
 
 function bindPageLinks() {
   $('.pagination a').click(function () {
-    $('html, body').animate({scrollTop:0}, 'fast');
+    $('html, body').animate({scrollTop: $('#story').offset().top}, 'fast');
   });
 }
