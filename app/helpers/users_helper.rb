6d6d6393530f96041e13db03868e2d79716dd219
Jori Hardman <jorihardman@gmail.com>
2011-02-22 16:36:09 -0600

    Minor change to format of follow button

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,9 +2,9 @@ module UsersHelper
   def follow(guide_id)
     if current_user.id != guide_id
       if UserRelationship.where({:follower_id => current_user.id, :guide_id => params[:id]}).first.nil?
-        return "#{button_to 'Follow', follow_author_path(params[:id]), :remote => true}"
+        return "<p>#{button_to 'Follow', follow_author_path(params[:id]), :remote => true}</p>"
       else
-        return "#{button_to 'Unfollow', unfollow_author_path(params[:id]), :remote => true, :method => :delete}"
+        return "<p>#{button_to 'Unfollow', unfollow_author_path(params[:id]), :remote => true, :method => :delete}</p>"
       end
     else
       return ''
