8d0691bacce73dc68efbe9e5b164b887440a1d4d
Philip Lee <rocketman768@gmail.com>
2011-03-02 12:47:42 -0600

    Returned the drop_seed_details code to original.

--- db/migrate/20110219183944_drop_seed_details.rb
+++ db/migrate/20110219183944_drop_seed_details.rb
@@ -1,9 +1,9 @@
 class DropSeedDetails < ActiveRecord::Migration
-  def self.down
+  def self.up
     drop_table "seed_details"
   end
 
-  def self.up
+  def self.down
     create_table "seed_details", :force => true do |t|
       t.integer  "seed_id"
       t.string   "title"
